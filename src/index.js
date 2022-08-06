fbfc70f3c56986233df7e64c0ca3a727cf2918c3
NikitaBarashkov <nik96barashkov@yandex.by>
2022-08-06 14:06:56 +0300

    feat: add function decode (Sat, 06 august 2022, 14:06)

--- src/index.js
+++ src/index.js
@@ -39,6 +39,62 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+
+    const firstArr = [];
+  
+  for(let i = 0; i < expr.length; i++){
+    firstArr.push(expr[i] + expr[i + 1]);
+    i += 1;
+  }
+  
+  const arrForFive = []
+
+  for(let i = 0; i < firstArr.length; ){
+    let smallArr = firstArr.slice(i, i + 5);
+    arrForFive.push(smallArr);
+    i += 5;
+  }
+
+  // let someArr;
+
+  arrForFive.forEach(arr => {
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] === '00'){
+        arr.splice(i, 1)
+        i -= 1;
+      } else if(arr[i] === '10'){
+        arr.splice(i, 1, '.');
+      } else if(arr[i] === '11'){
+        arr.splice(i, 1, '-')
+      } 
+    }
+    // console.log(arr.join())
+    // someArr += arr.join('')
+    // console.log(someArr)
+  })
+
+let arrayStr = []
+
+for(let i = 0; i < arrForFive.length; i++){
+  arrayStr.push(arrForFive[i].join(''));
+//   console.log(arrayStr[i])
+}
+
+const answer = []
+
+for(let i = 0; i < arrayStr.length; i++){
+  for(let key in MORSE_TABLE){
+    if(arrayStr[i] === key){
+      answer.push(MORSE_TABLE[key]);
+    } 
+  }
+  if(arrayStr[i] === '**********'){
+    answer.push(' ');
+  }
+}
+
+return answer.join('');
+
 }
 
 module.exports = {
